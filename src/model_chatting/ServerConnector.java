20203956b18172863a25c75e0d3d4e2dfdbe118a
Viet Huynh <huynhviet98@gmail.com>
2023-11-26 15:01:21 +0700

    +init server connector

---
+++ src/model_chatting/ServerConnector.java
@@ -0,0 +1,27 @@
+package model_chatting;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerConnector {
+    private Socket socket;
+    private PrintWriter out;
+
+    public ServerConnector(String serverAddress, int serverPort, String str) {
+        try {
+            this.socket = new Socket(serverAddress, serverPort);
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+}
+
+
+
